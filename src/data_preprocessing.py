3c0c46799162053dbc1ee57f4bc988326e296e15
efosazuwa <efosazuwa0@gmail.com>
2025-04-13 11:23:56 -0600

    complete data preprocessing pipeline

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -65,15 +65,90 @@ class DataProcessor:
             X = df.drop(columns=['booking_status'])
             y = df["booking_status"]
 
-            smote = SMOTE(random_state=30)
+            random_state = self.config["data_processing"]["random_state"]
+            smote = SMOTE(random_state=random_state)
             X_resampled, y_resampled = smote.fit_resample(X, y)
 
             balanced_df = pd.DataFrame(X_resampled, columns=X.columns)
             balanced_df["booking_status"] = y_resampled
 
-            logger.info("Balanced data shape: ", balanced_df.shape)
+            logger.info("Balanced data shape: ", extra={"balanced_data_shape": balanced_df.shape})
             return balanced_df
         
         except Exception as e:
             logger.error(f"Error during balancing step: {e}")
-            raise CustomException("Failed to balancee data", e)
+            raise CustomException("Failed to balance data", e)
+        
+    def feature_selection(self, df: pd.DataFrame):
+        try:
+            logger.info("Starting feature selection step")
+
+            X = df.drop(columns=['booking_status'])
+            y = df["booking_status"]
+
+            random_state = self.config["data_processing"]["random_state"]
+            model = RandomForestClassifier(random_state=random_state)
+            model.fit(X, y)
+
+            feature_importance = model.feature_importances_
+            feature_importance_df = pd.DataFrame({
+                "feature": X.columns,
+                "importance": feature_importance
+            })
+            feature_importance_df.sort_values(by="importance", ascending=False, inplace=True)
+
+            num_features = self.config["data_processing"]["no_of_features"]
+            top_features = feature_importance_df["feature"][:num_features].values
+            logger.info("Top Features", extra={"top_features": top_features})
+
+            target_variable = self.config["data_processing"]["target_column"]
+            top_features_df = df[top_features.tolist() + [target_variable]]
+
+            logger.info("Feature selection successful completed")
+            return top_features_df
+
+        except Exception as e:
+            logger.error(f"Error during feature selection step: {e}")
+            raise CustomException("Failed to select features data", e)
+        
+
+    def save_data(self, df: pd.DataFrame, file_path: str):
+        try:
+            logger.info(f"Saving data to {file_path}")
+            df.to_csv(file_path, index=False)
+            logger.info(f"Data saved successfully to {file_path}")
+
+        except Exception as e:
+            logger.error(f"Error during saving data: {e}")
+            raise CustomException("Failed to save data", e)
+        
+    def process_data(self):
+        try:
+            logger.info("Starting data processing pipeline")
+
+            train_df = load_data(self.train_path)
+            test_df = load_data(self.test_path)
+
+            train_df = self.preprocess_data(train_df)
+            test_df = self.preprocess_data(test_df)
+
+            train_df = self.balance_data(train_df)
+            test_df = self.balance_data(test_df)
+
+            train_df = self.feature_selection(train_df)
+            test_df = test_df[train_df.columns]
+
+            self.save_data(train_df, PROCESSED_TRAIN_DATA_PATH)
+            self.save_data(test_df, PROCESSED_TEST_DATA_PATH)
+
+            logger.info("Data processing pipeline completed successfully")
+
+        except Exception as e:
+            logger.error(f"Error during processing pipeline: {e}")
+            raise CustomException("Failed to complete processing pipeline", e)
+        
+if __name__ == "__main__":
+    processor = DataProcessor(TRAIN_FILE_PATH, TEST_FILE_PATH, PROCESSED_DIR, CONFIG_PATH)
+    processor.process_data()
+
+
